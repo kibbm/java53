15337e7dfb9235a7710e3758a3115514f6836b9a
bit <bit@bitcompu-e7d62f>
2014-07-23 19:41:55 +0900

    ..upload with code
    Change-Id: I0a51e46ec465613a2e8d8aea3033d2adda243656
    Signed-off-by: bit <bit@bitcompu-e7d62f>

---
+++ test01/src/test01/Hello.java
@@ -0,0 +1,26 @@
+package test01;
+
+public class Hello {
+
+	public Hello() {
+
+		System.out.println("hello~ SVN");
+	
+
+		System.out.println("반가워요.. 오호라..");
+	}
+	
+	public void m() {
+		System.out.println("연경이가 변경 추가함...");
+
+	}
+	
+	public void hi(){
+		System.out.println("안녕?");
+		
+		//System..//
+		
+		
+	}
+	
+}
